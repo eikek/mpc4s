36d47dbba8f2339118816cb8c22531c2af36025e
Eike Kettner <eike.kettner@posteo.de>
2018-09-23 00:51:40 +0200

    Player: Replace aweful slow `makeId` function with `Base64.encode`
    It's not only faster, but also allows to use the complete name of the
    album. The performance was noticeably degraded on low power devices,
    like my cheap 7 inch tablet.
    
    Issue: #19

--- modules/player/src/main/elm/Util/Html.elm
+++ modules/player/src/main/elm/Util/Html.elm
@@ -2,6 +2,7 @@ module Util.Html exposing (..)
 
 import Html.Attributes
 import Html exposing (Attribute)
+import Base64
 
 width: String -> Attribute msg
 width w =
@@ -9,15 +10,4 @@ width w =
 
 makeId: String -> String
 makeId str =
-    let
-        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-"
-    in
-        "Album-" ++ (String.foldr (appendIf <| String.toList chars) "" str)
-
-appendIf: List Char -> Char -> String -> String
-appendIf allowed ch str =
-    let
-        pre = if List.member ch allowed then ch else '-'
-    in
-        if pre == '-' && String.startsWith "-" str then str
-        else String.cons pre str
+    Base64.encode str
