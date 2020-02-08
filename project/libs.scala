4a54d7643b9b072084ce63db4183a0c316673db9
Scala Steward <me@scala-steward.org>
2020-02-08 02:16:51 +0100

    Update circe-core, circe-generic, ... to 0.13.0

--- project/libs.scala
+++ project/libs.scala
@@ -52,9 +52,9 @@ object libs {
 
   // https://github.com/circe/circe
   // ASL 2.0
-  val `circe-core` = "io.circe" %% "circe-core" % "0.10.1"
-  val `circe-generic` = "io.circe" %% "circe-generic" % "0.10.1"
-  val `circe-parser` = "io.circe" %% "circe-parser" % "0.10.1"
+  val `circe-core` = "io.circe" %% "circe-core" % "0.13.0"
+  val `circe-generic` = "io.circe" %% "circe-generic" % "0.13.0"
+  val `circe-parser` = "io.circe" %% "circe-parser" % "0.13.0"
 
   // http://tika.apache.org
   // ASL 2.0
